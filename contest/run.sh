99d045fc99bed030a2934b0222c3a1cba530c200
raincity <raincity343@outlook.com>
2025-02-21 15:11:52 +0800

    a

--- contest/run.sh
+++ contest/run.sh
@@ -2,7 +2,7 @@
 for i in *.in; do
   echo "$i"
   if ! diff -Z <(./main < $i) ${i%.*}.ans; then
-    echo "Error"
+    echo 'Error'
     exit 1
   fi
 done
--- contest/test.sh
+++ contest/test.sh
@@ -3,7 +3,7 @@ for i in {1..100000}; do
   echo "$i"
   ./gen > ex.in
   if ! diff -Z <(./main < ex.in) <(./std < ex.in); then
-    echo "Error"
+    echo 'Error'
     exit 1
   fi
 done
